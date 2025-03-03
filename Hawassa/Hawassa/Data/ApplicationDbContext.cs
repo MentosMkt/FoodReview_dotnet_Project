abc9dc00fded706796268d6978d0c03a4123205a
Mintesnot Markos Karrisa <nsr-1180-14@amu.edu.et>
2025-03-03 08:40:41 +0300

    Hawassa-Admin Page

---
+++ Hawassa/Hawassa/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Hawassa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hawassa.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddMenu> Menu { get; set; }
+    }
+}
---
+++ Hawassa/Hawassa/Models/Menuform.cs
@@ -0,0 +1,21 @@
+﻿ 
+using System.ComponentModel.DataAnnotations;
+
+namespace Hawassa.Models
+{
+    public class Menuform
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Location { get; set; } = "";
+        [Required, MaxLength(100)]
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; } = "";
+        [Required]
+        public Decimal Price { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+    }
+}
